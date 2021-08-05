eaaf6940272515161ca4dcd0c60bac99199d0a6c
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-08-05 12:12:39 +0300

    icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,28 @@
+import 'package:flutter/material.dart';
 
-helloy world
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
